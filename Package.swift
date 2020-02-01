73821bd5156e2e5dc18bc9b177b2cceaa50d64e0
Zyphrax <yvo@yvo.net>
2020-02-01 17:34:54 -0500

    Add Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "HTTPParserC",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9),
+        .macOS(.v10_10),
+        .watchOS(.v2)
+    ],
+    products: [
+        .library(
+            name: "HTTPParserC",
+            targets: ["HTTPParserC"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "HTTPParserC"
+        )
+    ]
+)
--- Sources/HTTPParserC/include/HTTPParserC.h
+++ Sources/HTTPParserC/include/HTTPParserC.h
@@ -14,4 +14,4 @@ FOUNDATION_EXPORT double HTTPParserC_VersionNumber;
 //! Project version string for HTTPParserC.
 FOUNDATION_EXPORT const unsigned char HTTPParserC_VersionString[];
 
-#import <HTTPParserC/http_parser.h>
+#import "http_parser.h"
